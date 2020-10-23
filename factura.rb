3c59dcc9e89722dec0efd4e0ee20f520483c4d2f
karencabezas <karencabezaslicera@gmail.com>
2020-10-23 07:55:24 +0000

    Se añaden los impuestos variables, solo para el estado CA

--- factura.rb
+++ factura.rb
@@ -1,23 +1,35 @@
-def descuentos(subtotal)
-    @descuentos ={1000 => 3, 5000 => 5, 7000 => 7,10000 => 10, 50000 => 15}
-    @tasadescuento = @descuentos[@descuentos.select{|monto,tasa| subtotal>=monto}.keys.max]
-    return subtotal*@tasadescuento
-end
+class Factura
+    def initialize(estado)
+        @estado = estado
+        @descuentos ={1000 => 3, 5000 => 5, 7000 => 7,10000 => 10, 50000 => 15}
+    end
 
-def facturas(cantidad,precioUnitario,p_impuesto)
-    subtotal=cantidad*precioUnitario
-    preciodescuento = descuentos(subtotal)*0.01
-    totaldescuento= (subtotal - preciodescuento)
-    total = totaldescuento + totaldescuento*p_impuesto*0.01
-    "******** FACTURA ********\n" +
-    "Precio Unitario: $ #{precioUnitario} \n" +
-    "Cantidad: #{cantidad} \n" +
-    "Descuento: #{@tasadescuento} % \n" +
-    "Impuesto: #{p_impuesto}% \n" +
-    "Total: $ #{total} " 
-end
+    def descuentos(subtotal)
+        @tasadescuento = @descuentos[@descuentos.select{|monto,tasa| subtotal>=monto}.keys.max]
+        return subtotal*@tasadescuento
+    end
 
+    def impuestos()
+        impuestos = {
+        "CA" => 5.36}
+        @tasaimpuesto = impuestos[@estado]
+    end
+
+    def facturas(cantidad,precioUnitario)
+        subtotal=cantidad*precioUnitario
+        preciodescuento = descuentos(subtotal)*0.01
+        totaldescuento= (subtotal - preciodescuento)
+        impuestos()
+        total = totaldescuento + totaldescuento*@tasaimpuesto*0.01
+        "******** FACTURA ********\n" +
+        "Precio Unitario: $ #{precioUnitario} \n" +
+        "Cantidad: #{cantidad} \n" +
+        "Descuento: #{@tasadescuento} % \n" +
+        "Impuesto: #{@tasaimpuesto} % \n" +
+        "Total: $ #{total} " 
+    end
+end
 cantidad = ARGV[0].to_i
 precioUnitario = ARGV[1].to_i
-p_impuesto = ARGV[2].to_i
-puts facturas(cantidad,precioUnitario,p_impuesto)
+estado = Factura.new(ARGV[2])
+puts estado.facturas(cantidad,precioUnitario)
