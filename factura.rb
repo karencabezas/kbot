10cc9e3cd6c43b5cc6ab37144418e67bd812e52f
karencabezas <karencabezaslicera@gmail.com>
2020-10-23 07:39:27 +0000

    Para cantidad=1 y precio=10  (valores fijos), considerando que no hay impuestos ni descuentos

--- factura.rb
+++ factura.rb
@@ -1,1 +1,2 @@
-puts "******** FACTURA ********\n" + "Precio Unitario: $  \n" + "Cantidad:  \n" +"Total:  $  " 
+total=10
+puts "******** FACTURA ********\n" + "Precio Unitario: $ 10 \n" + "Cantidad: 1 \n" +"Total:  $ #{total} " 
