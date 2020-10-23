26a6764ccd83e0d1fe62f9dfde3f85c1f0be00e5
karencabezas <karencabezaslicera@gmail.com>
2020-10-23 07:46:53 +0000

    Se ingresa el valor del descuento manualmente (ejemplo: 3,4,5..)

--- factura.rb
+++ factura.rb
@@ -1,16 +1,17 @@
-def facturas(cantidad,precioUnitario)
+def facturas(cantidad,precioUnitario,p_descuento)
     subtotal=cantidad*precioUnitario
-    preciodescuento = (subtotal)*0.03
+    preciodescuento = (subtotal)*p_descuento*0.01
     totaldescuento= (subtotal - preciodescuento)
     total = totaldescuento + totaldescuento*0.0825
     "******** FACTURA ********\n" +
     "Precio Unitario: $ #{precioUnitario} \n" +
     "Cantidad: #{cantidad} \n" +
-    "Descuento: 3% \n" +
+    "Descuento: #{p_descuento}% \n" +
     "Impuesto: 8.25% \n" +
     "Total: $ #{total} " 
 end
 
 cantidad = ARGV[0].to_i
 precioUnitario = ARGV[1].to_i
-puts facturas(cantidad,precioUnitario)
+p_descuento = ARGV[2].to_i
+puts facturas(cantidad,precioUnitario,p_descuento)
