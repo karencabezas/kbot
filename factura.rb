a1bfffa4aa43774f2a00bb2cd10fd38e2d427927
karencabezas <karencabezaslicera@gmail.com>
2020-10-23 07:49:13 +0000

    Se añaden los descuentos variables para subtotales mayores a 1000

--- factura.rb
+++ factura.rb
@@ -1,17 +1,22 @@
-def facturas(cantidad,precioUnitario,p_descuento)
+def descuentos(subtotal)
+    @descuentos ={1000 => 3}
+    @tasadescuento = @descuentos[@descuentos.select{|monto,tasa| subtotal>=monto}.keys.max]
+    return subtotal*@tasadescuento
+end
+
+def facturas(cantidad,precioUnitario)
     subtotal=cantidad*precioUnitario
-    preciodescuento = (subtotal)*p_descuento*0.01
+    preciodescuento = descuentos(subtotal)*0.01
     totaldescuento= (subtotal - preciodescuento)
     total = totaldescuento + totaldescuento*0.0825
     "******** FACTURA ********\n" +
     "Precio Unitario: $ #{precioUnitario} \n" +
     "Cantidad: #{cantidad} \n" +
-    "Descuento: #{p_descuento}% \n" +
+    "Descuento: #{@tasadescuento} % \n" +
     "Impuesto: 8.25% \n" +
     "Total: $ #{total} " 
 end
 
 cantidad = ARGV[0].to_i
 precioUnitario = ARGV[1].to_i
-p_descuento = ARGV[2].to_i
-puts facturas(cantidad,precioUnitario,p_descuento)
+puts facturas(cantidad,precioUnitario)
